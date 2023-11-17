02f002522b94600a6a04a9af4b6f4b11aef47e9d
oemelina <afema13@gmail.com>
2023-11-17 08:12:51 +0300

    lesson_3

---
+++ src/com/urise/webapp/storage/ArrayStorage.java
@@ -0,0 +1,75 @@
+package com.urise.webapp.storage;
+
+import com.urise.webapp.model.Resume;
+
+public class ArrayStorage {
+    private Resume[] storage = new Resume[10000];
+    private int size = 0;
+
+    public void clear() {
+        for (int i = 0; i < size; i++) {
+            storage[i] = null;
+            size = 0;
+        }
+    }
+    public void update(Resume r) {
+        int index = getIndex(r.getUuid());
+        if(index == -1) {
+            System.out.println("Resume " + r.getUuid() + " already exist");
+        } else {
+            storage[index] = r;
+        }
+    }
+
+    public void save(Resume r) {
+        if(getIndex(r.getUuid()) != -1) {
+            System.out.println("Resume " + r.getUuid() + " already exist");
+        } else if(size == storage.length) {
+            System.out.println("Storage overflow");
+        } else {
+            storage[size] = r;
+            size++;
+        }
+    }
+
+    public Resume get(String uuid) {
+        int index = getIndex(uuid);
+        if (index == -1) {
+            System.out.println("Resume " + uuid + " not exist");
+            return null;
+        }
+        return storage[index];
+    }
+
+    public void delete(String uuid) {
+        int index = getIndex(uuid);
+        if (index == -1) {
+            System.out.println("Resume " + uuid + " not exist");
+        } else {
+            storage[index] = storage[size - 1];
+            storage[size - 1] = null;
+            size--;
+          }
+        }
+
+    public Resume[] getAll() {
+        Resume[] result = new Resume[size];
+        for (int i = 0; i < size; i++) {
+            result[i] = storage[i];
+        }
+        return result;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    private int getIndex(String uuid) {
+        for (int i = 0; i < size; i++) {
+            if (uuid.equals(storage[i].getUuid())) {
+                return i;
+            }
+        }
+            return -1;
+    }
+  }
